719e9db9685a81fbabb1a7ced6dbe97215b25e8a
Antoine <antoine24deboeuf@gmail.com>
2024-06-24 16:49:43 +0200

    updated view for users

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -11,7 +11,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/login', name: 'app.login', methods: ['GET', 'POST'])]
+    #[Route('/', name: 'app.login', methods: ['GET', 'POST'])]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
 
