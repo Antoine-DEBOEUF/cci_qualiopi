c64dc6f0381c8776bb8e87802b3fab33c149b9ac
Antoine <antoine24deboeuf@gmail.com>
2024-07-05 10:04:17 +0200

    add new database file

--- database.sql
+++ database.sql
@@ -1,84 +1,224 @@
-DROP DATABASE IF EXISTS epicerie;
-#Premiere bdd
-create database epicerie;
-#se placer sur cette bdd
-use epicerie;
+-- MySQL dump 10.13  Distrib 8.0.35, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: cci_qualiopi
+-- ------------------------------------------------------
+-- Server version	8.0.35
 
-#creation des premieres tables
-create table poste(
-id int primary key auto_increment,
-nom VARCHAR(80) NOT NULL UNIQUE);
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE genre(
-id int primary key auto_increment,
-nom VARCHAR(80) NOT NULL UNIQUE);
+--
+-- Table structure for table `doctrine_migration_versions`
+--
 
-CREATE TABLE rayon(
-id int primary key auto_increment,
-nom VARCHAR(80) NOT NULL UNIQUE);
+DROP TABLE IF EXISTS `doctrine_migration_versions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int DEFAULT NULL,
+  PRIMARY KEY (`version`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE caisse(
-id int primary key auto_increment,
-matricule VARCHAR(11) NOT NULL UNIQUE);
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
 
-CREATE TABLE statut(
-id int primary key auto_increment,
-nom VARCHAR(50) NOT NULL);
+/*!40000 ALTER TABLE `doctrine_migration_versions` DISABLE KEYS */;
+INSERT INTO `doctrine_migration_versions` VALUES ('DoctrineMigrations\\Version20240617100721','2024-06-17 12:07:31',139),('DoctrineMigrations\\Version20240617120354','2024-06-17 14:04:02',60),('DoctrineMigrations\\Version20240617121356','2024-06-17 14:14:02',42),('DoctrineMigrations\\Version20240617122630','2024-06-17 14:26:37',93),('DoctrineMigrations\\Version20240619120913','2024-06-19 14:09:26',160),('DoctrineMigrations\\Version20240626094326','2024-06-26 11:43:39',139),('DoctrineMigrations\\Version20240626142623','2024-06-26 16:26:31',58),('DoctrineMigrations\\Version20240627122218','2024-06-27 14:22:31',55),('DoctrineMigrations\\Version20240627124539','2024-06-27 14:45:45',88);
+/*!40000 ALTER TABLE `doctrine_migration_versions` ENABLE KEYS */;
 
-CREATE TABLE personnel(
-id int primary key auto_increment,
-nom VARCHAR(80) NOT NULL,
-prenom VARCHAR(80) NOT NULL,
-id_poste INT NOT NULL,
-date_naiss DATE NOT NULL,
-date_entree DATE DEFAULT (NOW()),
-date_sortie DATE,
-id_genre INT NOT NULL,
-matricule VARCHAR(12) NOT NULL,
-id_manager INT);
+--
+-- Table structure for table `document`
+--
 
-CREATE TABLE infos_personnel(
-id_personnel INT PRIMARY KEY NOT NULL,
-tel VARCHAR(11),
-mail VARCHAR(150) UNIQUE,
-ville VARCHAR(70),
-adresse VARCHAR(100),
-CP VARCHAR(5),
-secu VARCHAR(13) UNIQUE,
-iban VARCHAR(34));
+DROP TABLE IF EXISTS `document`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `document` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `module_id` int DEFAULT NULL,
+  `file_name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file_size` int DEFAULT NULL,
+  `categorie` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `titre` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_D8698A76AFC2B591` (`module_id`),
+  KEY `IDX_D8698A76A76ED395` (`user_id`),
+  CONSTRAINT `FK_D8698A76A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_D8698A76AFC2B591` FOREIGN KEY (`module_id`) REFERENCES `module` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE produit(
-id INT PRIMARY KEY AUTO_INCREMENT,
-nom VARCHAR(100) NOT NULL,
-des TEXT,
-prix DECIMAL(10,2) NOT NULL,
-id_rayon INT NOT NULL);
+--
+-- Dumping data for table `document`
+--
 
-CREATE TABLE adherent(
-id INT PRIMARY KEY AUTO_INCREMENT,
-ncarte VARCHAR(13) NOT NULL,
-nom VARCHAR(80) NOT NULL,
-prenom VARCHAR(80) NOT NULL,
-date_adhesion DATETIME DEFAULT( NOW()));
+/*!40000 ALTER TABLE `document` DISABLE KEYS */;
+INSERT INTO `document` VALUES (11,20,1,'itineraire-pedagogique-format-excel-maj-fevrier-2024-667d4efbd0d9e350873746.xlsx',31664,'itin_pedago',NULL),(13,19,NULL,'itineraire-pedagogique-format-excel-maj-fevrier-2024-667d5f95d2740871475975.xlsx',31664,'modele','Itinéraire pédagogique');
+/*!40000 ALTER TABLE `document` ENABLE KEYS */;
 
-CREATE TABLE infos_adherent(
-id INT PRIMARY KEY,
-mail VARCHAR(180),
-tel VARCHAR(11),
-adresse VARCHAR(180),
-CP VARCHAR(5),
-id_genre INT NOT NULL);
+--
+-- Table structure for table `formation`
+--
 
-CREATE TABLE passage_caisse(
-id INT PRIMARY KEY AUTO_INCREMENT,
-date_passage DATETIME DEFAULT(NOW()),
-id_adherent INT NOT NULL,
-id_caisse INT NOT NULL,
-id_pers INT NOT NULL,
-id_statut INT NOT NULL);
+DROP TABLE IF EXISTS `formation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `formation` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `site_id` int NOT NULL,
+  `intitule` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_debut` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `date_fin` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_404021BFF6BD1646` (`site_id`),
+  CONSTRAINT `FK_404021BFF6BD1646` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE ticket(
-id INT PRIMARY KEY AUTO_INCREMENT,
-id_passage_caisse INT NOT NULL,
-id_produit INT NOT NULL,
-qte INT NOT NULL);
+--
+-- Dumping data for table `formation`
+--
+
+/*!40000 ALTER TABLE `formation` DISABLE KEYS */;
+INSERT INTO `formation` VALUES (1,4,'forma1','2024-06-19 00:00:00','2027-06-25 00:00:00'),(2,4,'forma2','2024-06-13 00:00:00','2028-05-16 00:00:00');
+/*!40000 ALTER TABLE `formation` ENABLE KEYS */;
+
+--
+-- Table structure for table `messenger_messages`
+--
+
+DROP TABLE IF EXISTS `messenger_messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messenger_messages` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `body` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `headers` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `queue_name` varchar(190) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `created_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `available_at` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `delivered_at` datetime DEFAULT NULL COMMENT '(DC2Type:datetime_immutable)',
+  PRIMARY KEY (`id`),
+  KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  KEY `IDX_75EA56E016BA31DB` (`delivered_at`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `messenger_messages`
+--
+
+/*!40000 ALTER TABLE `messenger_messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `messenger_messages` ENABLE KEYS */;
+
+--
+-- Table structure for table `module`
+--
+
+DROP TABLE IF EXISTS `module`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `module` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `site_id` int NOT NULL,
+  `intitule` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date_debut` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `date_fin` datetime NOT NULL COMMENT '(DC2Type:datetime_immutable)',
+  `formation_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `IDX_C242628F6BD1646` (`site_id`),
+  KEY `IDX_C2426285200282E` (`formation_id`),
+  KEY `IDX_C242628A76ED395` (`user_id`),
+  CONSTRAINT `FK_C2426285200282E` FOREIGN KEY (`formation_id`) REFERENCES `formation` (`id`),
+  CONSTRAINT `FK_C242628A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`),
+  CONSTRAINT `FK_C242628F6BD1646` FOREIGN KEY (`site_id`) REFERENCES `site` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `module`
+--
+
+/*!40000 ALTER TABLE `module` DISABLE KEYS */;
+INSERT INTO `module` VALUES (1,20,4,'module1.3','2024-06-19 00:00:00','2024-10-18 00:00:00',1),(2,21,4,'module2','2024-06-21 00:00:00','2024-10-17 00:00:00',2);
+/*!40000 ALTER TABLE `module` ENABLE KEYS */;
+
+--
+-- Table structure for table `site`
+--
+
+DROP TABLE IF EXISTS `site`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `site` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `ville` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `code_postal` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `site`
+--
+
+/*!40000 ALTER TABLE `site` DISABLE KEYS */;
+INSERT INTO `site` VALUES (4,'Lyon',69009);
+/*!40000 ALTER TABLE `site` ENABLE KEYS */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `description` longtext COLLATE utf8mb4_unicode_ci,
+  `roles` json NOT NULL,
+  `nom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `prenom` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (19,'admin@test.com','$2y$13$5CouBk/Be1rizl2PehwmduvcnB8J5gAO5D9KvGhZhx7mpiMGPqIXC',NULL,'[\"ROLE_ADMIN\"]','test','admin'),(20,'user@test.com','$2y$13$xZ7JkkR3DCVBSJx21ZnjB.X38TkGljQPdniIN91pF9wn.claTKFxW',NULL,'[\"ROLE_USER\"]','test','user'),(21,'user2@test.com','$2y$13$KWMuWgOLe1ZZAnAkpIv36eNf0/bgzgCoA9Nj0eB6hL/EG9z8nBypO',NULL,'[\"ROLE_USER\"]','Test','user 2');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+
+--
+-- Dumping routines for database 'cci_qualiopi'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-07-05 10:02:56
